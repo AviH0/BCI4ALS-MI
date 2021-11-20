f6f4aacf7a22670aea386b9621e566864302e8a1
yarden <yarden11_2@walla.com>
2021-11-20 19:32:15 +0200

    updating the Laplacian function + my part of week 6 assignement

--- LaPlacian.m
+++ LaPlacian.m
@@ -1,22 +1,30 @@
-function [main_chan_filtered] = LaPlacian(data,main_chan,chan_nearby, all_channels)
+function [data_bef,data_aft] = LaPlacian(data,main_chan,chan_nearby, all_channels)
 % Recives data matrix (N channels x T time steps), 
-% the number of main channel and the vector of the nearby
+% the number of main channel/s and the vector 
+% (or matrix in case of a few filters, in which each row is composed) of the nearby
 % channels. It subtracts the nearby channels mean signal from the main one.
-% the output is only the filtered signal from the main channel.
+% the output is the data matrix without and after the change in the
+% filtered signal from the main channel.
 % If recives channels input as characters - need the 4th input- 
-% vector of all channel names. TEST
-
+% vector of all channel names.
+data_bef=data; data_aft=data_bef;
 main_chan_num=main_chan;
-vec_num_nearby=chan_nearby;
+num_nearby=chan_nearby;
+len_data_time=size(data,2);
 
     if ~isnumeric(main_chan)
         main_chan_num=find(all(ismember(all_channels,main_chan),2));
     end
     if ~isnumeric(chan_nearby)
-            vec_num_nearby=find(all(ismember(all_channels,chan_nearby),2));
+            num_nearby=find(all(ismember(all_channels,chan_nearby),2));
     end
+n_lap_filters=length(main_chan_num);
+main_chan_filtered=zeros(n_lap_filters,len_data_time);
 
-    % Main calculation of the Laplacian
-    main_chan_filtered = data(main_chan_num,:) - ...
-                            mean(data(vec_num_nearby,:));
+        for filt=1:n_lap_filters %loop over the number of filters to apply
+            % Main calculation of the Laplacian
+           main_chan_filtered(filt,:)=data(main_chan_num(filt),:) - ...
+               mean(data(num_nearby(filt,:))); 
+        end
+    data_aft(main_chan_num,:)=main_chan_filtered;
 end
--- WK6_Assignment.m
+++ WK6_Assignment.m
@@ -8,7 +8,25 @@ addpath(genpath('C:\Toolboxes\liblsl-Matlab'))
 addpath(genpath('C:\Recordings'))
 
 %% Define subject ID
-subjectID = "1_20211104"; % as entered at time of recording
+subjectID = "2021-11-14"; % as entered at time of recording
 
 %% read data, output raw and preprocessed files
 [EEG_raw, EEG_clean] = open_and_preprocess(subjectID);
+
+%% 2
+recording_folder=strcat("C:\Recordings\",subjectID,'\');
+MI3_segmentation(recording_folder);
+% a
+MI4_featureExtraction(recording_folder)
+
+%% b
+load('EEG_chans.mat');
+[bef_dat,aft_dat]=LaPlacian(EEG_clean.data, ['C04','C03'], ...
+    [5:2:11; 4:2:10], EEG_chans);
+
+folder_lap=strcat(recording_folder,'Lap\'); %Folder for data after Laplacian
+EEG_data=aft_dat;
+save(fullfile(folder_lap, 'cleaned_sub.mat'), 'EEG_data', '-mat');
+    %after copying EEG_events, EEG_chans & trainingVec to the lap folder(!)
+MI3_segmentation(folder_lap)
+MI4_featureExtraction(folder_lap)
